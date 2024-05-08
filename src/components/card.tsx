9b7121ddf801ce43c61e31eb40c73de6c14f2168
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2024-05-08 13:49:53 -0600

    🐛 [main] show real state photo url

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,12 +1,13 @@
 import { RealState } from "../types/real-state"
+import { DEFAULT_IMAGE } from "../utils/mocks/real-states"
 
 export default function Card({ info }: {
   info: RealState
 }) {
-  const { location, description, owner, listingPrice } = info
+  const { location, description, owner, listingPrice, photoUrl } = info
   return (
     <article className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-      <img className="object-cover h-full w-48 rounded-none rounded-s-lg" src="https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3A0d37867b-d0a8-44b7-8c62-a1c73809e34e?source=next-article&fit=scale-down&quality=highest&width=1440&dpr=1" alt="" />
+      <img className="object-cover h-full w-48 rounded-none rounded-s-lg" src={photoUrl ?? DEFAULT_IMAGE} alt={location} />
       <div className="flex flex-col justify-between p-4 leading-normal">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{location}</h5>
         <span>{owner}</span>
--- src/components/publish-card.tsx
+++ src/components/publish-card.tsx
@@ -5,7 +5,7 @@ import { togglePublishRealState } from "../state/redux/realStateReducer"
 export default function PublishCard({ info }: {
   info: RealState
 }) {
-  const { location, description, owner, listingPrice, isPublished, id } = info
+  const { location, description, owner, listingPrice, isPublished, id, photoUrl } = info
 
   const dispatch = useDispatch()
   const togglePublish = () => {
@@ -13,7 +13,7 @@ export default function PublishCard({ info }: {
   }
   return (
     <article className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-      <img className="object-cover h-full w-48 rounded-none rounded-s-lg" src="https://www.ft.com/__origami/service/image/v2/images/raw/ftcms%3A0d37867b-d0a8-44b7-8c62-a1c73809e34e?source=next-article&fit=scale-down&quality=highest&width=1440&dpr=1" alt="" />
+      <img className="object-cover h-full w-48 rounded-none rounded-s-lg" src={photoUrl} alt={location} />
       <div className="flex flex-col justify-between p-4 leading-normal">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{location}</h5>
         <span>{owner}</span>
