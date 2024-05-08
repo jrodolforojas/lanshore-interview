0b648c73646e12228ea0d0a9129c7a6c8e16ed6e
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2024-05-08 11:25:38 -0600

    ✨ [main] pages

---
+++ src/pages/publish-listing.tsx
@@ -0,0 +1,7 @@
+export default function PublishListing() {
+  return (
+    <section>
+      Publish Listing
+    </section>
+  )
+}
---
+++ src/pages/submit-listing.tsx
@@ -0,0 +1,7 @@
+export default function SubmitListing() {
+  return (
+    <section>
+      Submit Listing
+    </section>
+  )
+}
