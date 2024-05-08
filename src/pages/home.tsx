f496a3ea0fb42675b45f9014a89d02de9027f702
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2024-05-08 12:21:28 -0600

    ♻️ [main] get real states from global state

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,15 @@
+import { useSelector } from "react-redux";
 import Card from "../components/card";
-import { realStates } from "../utils/mocks/real-states";
+import { RealState } from "../types/real-state";
 
 export default function Home() {
+  const realStates = useSelector((state: RealState[]) => state)
+  const publishedRealStates = realStates.filter((realStates) => realStates.isPublished)
+
   return (
     <section className="flex w-full justify-center">
       <ul className="gap-y-5 flex flex-col">
-        {realStates.filter((realStates) => realStates.isPublished).map((realState) =>
+        {publishedRealStates.map((realState) =>
           <Card key={realState.id} info={realState} />
         )}
       </ul>
