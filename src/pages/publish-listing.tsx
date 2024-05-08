bdf4ca241a259242797feed1e4346b36b9894072
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2024-05-08 12:57:18 -0600

    💥 [main] publish listings

--- src/pages/publish-listing.tsx
+++ src/pages/publish-listing.tsx
@@ -1,7 +1,19 @@
+import { useSelector } from "react-redux"
+import { RealState } from "../types/real-state"
+import PublishCard from "../components/publish-card"
+
 export default function PublishListing() {
+  const allRealStates = useSelector((state: RealState[]) => state)
   return (
-    <section>
-      Publish Listing
+    <section className="flex w-full justify-center flex-col items-center max-w-3xl mx-auto">
+      <header className="mb-5">
+        <h2 className="text-xl font-bold text-orange-600">Publish/Unpublish Listing</h2>
+      </header>
+      <ul className="gap-y-5 flex flex-col">
+        {allRealStates.map((realState) =>
+          <PublishCard key={realState.id} info={realState} />
+        )}
+      </ul>
     </section>
   )
 }
