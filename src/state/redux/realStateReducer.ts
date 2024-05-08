ce3621edf971c307139f377cbfc0e9b704c47b49
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2024-05-08 12:56:01 -0600

    🗃️ [main] toggle real state

--- src/state/redux/realStateReducer.ts
+++ src/state/redux/realStateReducer.ts
@@ -13,9 +13,19 @@ export const realStateSlice = createSlice({
       const newRealState = action.payload as RealState
       state.push(newRealState)
       toast.success('New Real State created')
+    },
+    togglePublishRealState: (state, action) => {
+      const id = action.payload
+      const currentRealStateIndex = state.findIndex((realState) => realState.id === id)
+      if (currentRealStateIndex === -1) return
+      const currentIsPublish = state[currentRealStateIndex].isPublished
+      state[currentRealStateIndex].isPublished = !currentIsPublish
+
+      if (currentIsPublish) toast.success('Unpublished successfully')
+      if (!currentIsPublish) toast.success('Published successfully')
     }
   }
 })
 
-export const { createRealState } = realStateSlice.actions
+export const { createRealState, togglePublishRealState } = realStateSlice.actions
 export default realStateSlice.reducer
