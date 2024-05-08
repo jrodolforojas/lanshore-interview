3a805b64bc3a97060658a99df2ffefb95390bf88
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2024-05-08 11:44:47 -0600

    💥 [main] home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,14 @@
+import Card from "../components/card";
+import { realStates } from "../utils/mocks/real-states";
+
 export default function Home() {
   return (
-    <section>Home Section</section>
+    <section className="flex w-full justify-center">
+      <ul className="gap-y-5 flex flex-col">
+        {realStates.filter((realStates) => realStates.isPublished).map((realState) =>
+          <Card key={realState.id} info={realState} />
+        )}
+      </ul>
+    </section>
   )
 }
