cbbe2fa8885b6e3aa061a7199f8d60454c52adfb
Jose Rodolfo Rojas <jrodolforojas1@gmail.com>
2024-05-08 11:43:23 -0600

    🤡 [main] real-state

---
+++ src/utils/mocks/real-states.ts
@@ -0,0 +1,28 @@
+import { RealState } from "../../types/real-state";
+
+export const realStates: RealState[] = [
+  {
+    id: '1',
+    owner: 'Ricardo M',
+    location: 'Perez Zeledón',
+    description: '2 bed, 2 bath house with small front yard. Beautiful blueish color.',
+    listingPrice: 500000,
+    isPublished: true
+  },
+  {
+    id: '2',
+    owner: 'Ricardo M',
+    location: 'Perez Zeledón',
+    description: '2 bed, 2 bath house with small front yard. Beautiful blueish color.',
+    listingPrice: 500000,
+    isPublished: false
+  },
+  {
+    id: '3',
+    owner: 'Rodolfo Rojas',
+    location: 'San Carlos',
+    description: '2 bed, 2 bath house with small front yard. Beautiful blueish color.',
+    listingPrice: 500000,
+    isPublished: true
+  }
+]
